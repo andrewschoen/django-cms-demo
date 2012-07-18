261623e52827322cf20fa0e833f9bb5fc0695b7c
Andrew Schoen <andrew.schoen@gmail.com>
2012-07-18 13:42:52 -0500

    added the polls app

---
+++ demo/polls/models.py
@@ -0,0 +1,22 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __unicode__(self):
+        return self.question
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
+    def __unicode__(self):
+        return self.choice
