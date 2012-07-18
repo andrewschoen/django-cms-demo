4aced27ca67321fc657fa3387e59db71dafa8e0a
Andrew Schoen <andrew.schoen@gmail.com>
2012-07-18 11:57:33 -0500

    django and django CMS project setup

---
+++ demo/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+) + urlpatterns
