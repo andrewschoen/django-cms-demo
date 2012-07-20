1be7146af3bc622e77cef0a68de3c19567866e50
Andrew Schoen <andrew.schoen@gmail.com>
2012-07-20 17:14:17 -0500

    added in bootstrap for the templates

--- demo/polls/views.py
+++ demo/polls/views.py
@@ -5,20 +5,6 @@ from django.template import RequestContext
 
 from polls.models import Choice, Poll
 
-# def index(request):
-#     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-#     return render_to_response('polls/index.html', 
-#         {'latest_poll_list': latest_poll_list})
-
-# def detail(request, poll_id):
-#     p = get_object_or_404(Poll, pk=poll_id)
-#     return render_to_response('polls/detail.html', {'poll': p},
-#                                context_instance=RequestContext(request))
-
-# def results(request, poll_id):
-#     p = get_object_or_404(Poll, pk=poll_id)
-#     return render_to_response('polls/results.html', {'poll': p})
-
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     try:
